9ea11e2d15b2c4cd66dd4b7ef604b1668d3ca110
cole <cole@coles-MacBook-Pro.local>
2024-07-16 12:25:31 -0700

    add SPLIT_WATCHDOG_ENABLE for lulu to allow split detection on boot

--- keyboards/boardsource/lulu/rp2040/config.h
+++ keyboards/boardsource/lulu/rp2040/config.h
@@ -1,6 +1,7 @@
 // Copyright 2022 jack (@waffle87)
 // SPDX-License-Identifier: GPL-2.0-or-later
 #pragma once
+#define SPLIT_WATCHDOG_ENABLE
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 200U
 #define SERIAL_USART_FULL_DUPLEX
